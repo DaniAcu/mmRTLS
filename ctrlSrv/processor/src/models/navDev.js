b286feb192a5b065d6c665278c96ae09eeddc380
Felipe Diniello <felipe.diniello@globant.com>
2021-07-14 16:10:26 -0300

    rm javascript

--- ctrlSrv/processor/src/models/navDev.js
+++
@@ -1,9 +0,0 @@
-class NavDev { 
-    constructor(mac, onboardingDate, lastConnected) {
-        this.mac = mac
-        this.onboardingDate = onboardingDate
-        this.lastConnected = lastConnected
-    }
-}
-
-export { NavDev }
