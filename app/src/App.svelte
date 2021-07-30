f254be3e7bca170121c6cd5fd44353cc98366b03
Daniel Acuña <danielnahuelacuna98@gmail.com>
2021-07-30 18:37:32 -0300

    chore: first commit ui app

---
+++ app/src/App.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import { map } from "./map";
+
+	console.log();
+</script>
+
+<main>
+	<h1>Map</h1>
+	<div class="map" use:map></div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+		margin: 0;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+
+	.map {
+		height: 32rem;
+	}
+</style>
---
+++ app/src/map.ts
@@ -0,0 +1,21 @@
+import * as L  from 'leaflet'
+import 'leaflet/dist/leaflet.css';
+
+const MOCK_IMAGE = "./img/indoor-map.png";
+
+export function map(node) {
+	const map = L.map(node, {
+      crs: L.CRS.Simple
+  });
+
+  const bounds: L.LatLngBoundsExpression = [[0,0], [1000,1000]];
+  L.imageOverlay(MOCK_IMAGE, bounds).addTo(map);
+  map.fitBounds(bounds);
+
+	return {
+		destroy() {
+			map.off();
+			map.remove();
+		}
+	};
+}
