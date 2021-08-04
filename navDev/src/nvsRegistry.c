bf0eea6312d177c5c2eb7dc5c7a934f1fe2f6155
kmilo17pet <kmilo17pet@gmail.com>
2021-08-04 14:05:57 -0500

    small fixes for consistency on nvsRegistry

--- navDev/src/nvsRegistry.c
+++ navDev/src/nvsRegistry.c
@@ -11,13 +11,15 @@
 
 
 #define STORAGE_NAMESPACE "storage"
+static const char *TAG = "NVM Storage";
 
-int32_t initializeNVSRegistry(void) {
-    // Initialize NVS
-    esp_err_t err = nvs_flash_init();
+int32_t initializeNVSRegistry(void)
+{
+    esp_err_t err = nvs_flash_init(); /* Initialize NVS */
+
     if ((ESP_ERR_NVS_NO_FREE_PAGES == err)) {
-        // NVS partition was truncated and needs to be erased, retry nvs_flash_init
-        ESP_ERROR_CHECK(nvs_flash_erase());
+        
+        ESP_ERROR_CHECK( nvs_flash_erase() ); /* NVS partition was truncated and needs to be erased, retry nvs_flash_init */
         err = nvs_flash_init();
     }
     ESP_ERROR_CHECK( err );
@@ -25,14 +27,15 @@ int32_t initializeNVSRegistry(void) {
     return err;
 }
 
-int32_t getValueFromNVSRegistry(const char *key, void *outValue, NvsRegistryDataType dataType, size_t *length) {
+int32_t getValueFromNVSRegistry(const char *key, void *outValue, NvsRegistryDataType dataType, size_t length)
+{
     nvs_handle handle;
-    esp_err_t err = nvs_open(STORAGE_NAMESPACE,NVS_READONLY , &handle); //NVS_READWRITE
+    esp_err_t err = nvs_open(STORAGE_NAMESPACE,NVS_READONLY , &handle); /*NVS_READWRITE*/
+
     if (err != ESP_OK) {
-        ESP_LOGE( TAG, "Error (%s) opening NVS handle!", esp_err_to_name(err));
+        ESP_LOGE( TAG, "Error (%s) opening NVS handle!", esp_err_to_name(err) );
         return err;
     } else {
-        printf("Done\n");
         switch (dataType) {
             case NvsRegistryDataType8:
                 err = nvs_get_i8(handle, key, outValue);
@@ -47,10 +50,13 @@ int32_t getValueFromNVSRegistry(const char *key, void *outValue, NvsRegistryData
                 err = nvs_get_i64(handle, key, outValue);
                 break;
             case NvsRegistryDataTypeStr:
-                err = nvs_get_str(handle, key, outValue, length);
+                err = nvs_get_str(handle, key, outValue, &length);
                 break;
         }
-        printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
+        if( ESP_OK == err ){
+            ESP_LOGI( TAG, "Failed, Error (%s)", esp_err_to_name(err) );
+        }
+        
         nvs_close(handle);
     }
 
@@ -58,14 +64,13 @@ int32_t getValueFromNVSRegistry(const char *key, void *outValue, NvsRegistryData
 }
 
 int32_t setValueToNVSRegistry(const char *key, void *inValue, NvsRegistryDataType dataType) {
-    //TBD
     nvs_handle handle;
     esp_err_t err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &handle);
-    if (err != ESP_OK) {
-         printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
+    if ( ESP_OK != err ) {
+        ESP_LOGE( TAG, "Error (%s) opening NVS handle!\n", esp_err_to_name(err) );
         return err;
     }else {
-        printf("Done\n");
+        ESP_LOGI( TAG, "Done" );
         switch (dataType) {
             case NvsRegistryDataType8:
                 err = nvs_set_u8(handle, key, *((uint8_t*)inValue));
@@ -83,10 +88,12 @@ int32_t setValueToNVSRegistry(const char *key, void *inValue, NvsRegistryDataTyp
                 err = nvs_set_str(handle, key, (char*)inValue);
                 break;
         }
-        if (err != ESP_OK) {
-            printf("Committing updates in NVS ... ");
+        if ( ESP_OK != err ) {
+            ESP_LOGI( TAG, "Committing updates in NVS...");
             err = nvs_commit(handle);
-            printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
+            if( ESP_OK == err ){
+                ESP_LOGI( TAG, "Failed, Error (%s)", esp_err_to_name(err) );
+            }
         }
         nvs_close(handle);
     }
@@ -94,64 +101,61 @@ int32_t setValueToNVSRegistry(const char *key, void *inValue, NvsRegistryDataTyp
     return ESP_OK;
 }
 
-//saving <inputData> blob to NVS
-int32_t setDataBlockRawToNvs(void *input, size_t len, const char *key){
+
+int32_t setDataBlockRawToNvs( const char *key, void *input, size_t len  )
+{
     nvs_handle_t my_handle;
     uint8_t* inputData = (uint8_t*)input;
     
-    // open
     esp_err_t err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &my_handle);
-    if (err != ESP_OK) {
-         printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
+    if ( ESP_OK != err ) {
+        ESP_LOGE( TAG, "Error (%s) opening NVS handle!\n", esp_err_to_name(err));
         return err;
     }
 
     err = nvs_set_blob(my_handle, key, inputData, len);
 
-    if (err != ESP_OK) {
-        printf("Error (%s) saving <inputData> blob to NVS!\n", esp_err_to_name(err));
+    if ( ESP_OK != err ) {
+        ESP_LOGE( TAG, "Error (%s) saving <inputData> blob to NVS!\n", esp_err_to_name(err));
         return err;
     }
 
-    // Commit
     err = nvs_commit(my_handle);
-    if (err != ESP_OK) {
-         printf("Error (%s) committing updates in NVS!\n", esp_err_to_name(err));
+    if ( ESP_OK != err ) {
+        ESP_LOGE( TAG, "Error (%s) committing updates in NVS!\n", esp_err_to_name(err));
         return err;
     }
 
-    // Close
     nvs_close(my_handle);
     return ESP_OK;
 }
 
-//blob-read data from NVS
-int32_t getDataBlockRawFromNvs(void *output, size_t *len, const char *key){
+
+int32_t getDataBlockRawFromNvs( const char *key, void *output, size_t len )
+{
     nvs_handle_t my_handle;
     
-    // open
     esp_err_t err = nvs_open(STORAGE_NAMESPACE, NVS_READWRITE, &my_handle);
-    if (err != ESP_OK) {
-        printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
+    if ( ESP_OK != err ) {
+        ESP_LOGE( TAG, "Error (%s) opening NVS handle!", esp_err_to_name(err));
         return err;
     }
 
-    err = nvs_get_blob(my_handle, key , NULL, len);
-    if (err != ESP_OK && err != ESP_ERR_NVS_NOT_FOUND)  {
-        printf("Error (%s) reading data from NVS!\n", esp_err_to_name(err));
+    err = nvs_get_blob(my_handle, key , NULL, &len);
+    if ( ( ESP_OK != err )  && ( ESP_ERR_NVS_NOT_FOUND != err ) ) {
+        ESP_LOGE( TAG, "Error (%s) reading data from NVS!", esp_err_to_name(err) );
         return err;
     }
-    // Read 
-    if (*len > 0) {
-        err = nvs_get_blob(my_handle, key, output, len);
+    
+    if ( len > 0u ) {
+        err = nvs_get_blob(my_handle, key, output, &len);
     }
 
-    if (err != ESP_OK) {
-        printf("Error (%s) reading data from NVS!\n", esp_err_to_name(err));
+    if ( ESP_OK != err ) {
+        ESP_LOGE( TAG, "Error (%s) reading data from NVS!", esp_err_to_name(err) );
         return err;
     }
 
-    // Close
     nvs_close(my_handle);
     return ESP_OK;
 }
