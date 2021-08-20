4070be08f9409cf3b4a2b20fb092f69bdf2a2890
Marcelo Lorenzati <mlorenzati@gmail.com>
2021-08-20 16:38:45 -0300

    App: build import fixes

--- ctrlSrv/app/src/streams/beacons.ts
+++ ctrlSrv/app/src/streams/beacons.ts
@@ -1,7 +1,8 @@
 import { catchError, map, Observable, of } from "rxjs";
 import { fromFetch } from "rxjs/fetch";
 import type { Beacon } from "src/interfaces/beacon.interface";
-import { Marker, MarkerType } from "./marker.types";
+import { MarkerType } from "./marker.types";
+import type { Marker } from "./marker.types";
 
 export const BEACON_ICON_URL = './static/markers/antenna.png';
 
