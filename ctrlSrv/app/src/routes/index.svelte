407e38e65706d6f5de574c596f208b5435f1969e
Gonzalo <gonzmartinez96@gmail.com>
2021-08-26 15:06:03 -0300

    added preloader for index view

--- ctrlSrv/app/src/routes/index.svelte
+++ ctrlSrv/app/src/routes/index.svelte
@@ -1,41 +1,51 @@
+<script lang="ts" context="module">
+  import type { LoadInput, LoadOutput } from "@sveltejs/kit";
+  import { mapBackgroundImage, mapMaxPosition } from "../store/map-background-image.store";
+
+  const UPLOAD_MAP_ROUTE = '/upload-map';
+
+  export const load: (input: LoadInput) => Promise<LoadOutput<{
+    mapSize: IIndoorPosition;
+    backgroundImage: string;
+  }>> = async () => {
+    const config = mapMaxPosition.value;
+    const backgroundImage = mapBackgroundImage.value;
+    const configIsLoaded = !!config;
+		return new Promise((resolve) => {
+      resolve(configIsLoaded && !!backgroundImage ? {
+        status: 200,
+        props: {
+          mapSize: config as IIndoorPosition,
+          backgroundImage
+        }
+      } : {
+        status: 303,
+        redirect: UPLOAD_MAP_ROUTE
+      });
+    })
+	}
+</script>
+
 <script lang="ts">
   import Marker from "../components/Map/Marker.svelte";
   import Map from "../components/Map/Map.svelte";
   import { onMount$ } from "../utils/lifecycles"
   import { getMarkers } from "../streams/markers";
-  import { filter, Observable, startWith, take, tap } from "rxjs";
-  import { mapBackgroundImage, mapMaxPosition } from "../store/map-background-image.store";
-  import { goto } from "$app/navigation";
+  import { startWith } from "rxjs";
   import type { IIndoorPosition } from "src/interfaces/position.interface";
 
-  const UPLOAD_MAP_ROUTE = '/upload-map';
+  export let mapSize: IIndoorPosition;
+  export let backgroundImage: string;
 
   const markers = onMount$.pipe(
     getMarkers,
     startWith([])
   );
 
-  const navigateToMapSetup = () => {
-    goto(UPLOAD_MAP_ROUTE);
-  }
-
-  const backgroundImage = mapBackgroundImage.pipe(
-    take(1)
-  );
-
-  const mapDimensions = (mapMaxPosition as Observable<IIndoorPosition>).pipe(
-    tap(dimensions => {
-      if (!dimensions) {
-        navigateToMapSetup();
-      }
-    }),
-    filter(dimensions => !!dimensions),
-
-  )
 </script>
 
 <div class="container">
-  <Map backgroundImage={$backgroundImage} mapSize={$mapDimensions}>
+  <Map {backgroundImage} {mapSize}>
     {#each $markers as {lat, lng, id, icon} (id)}
       <Marker x={lat} y={lng} {id} {icon}/>
     {/each}
